7c224e6d03a8ad62ec9b831f8573008ede56d0e6
Chandru Manoharan <chandru@Chandrus-MacBook-Pro.local>
2019-01-01 22:08:53 -0500

    sample verification for paypal ipn

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 const express = require('express');
-const configurePaypal = require('./src/configure');
+const bodyParser = require('body-parser');
+const queryString = require('querystring');
+const request = require('request');
+const util = require('util');
+const requestAsync = util.promisify(request);
 
+const configurePaypal = require('./src/configure');
 configurePaypal();
 
 const app = express();
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
 
 app.get('/', (request, response) => {
     response.send('Hello World');
 });
 
-app.post('/payment', (request, response) => {
+app.post('/payment', async (request, response) => {
     console.log('webhook received...');
+    const body = request.body || {};
     response.sendStatus(200);
+    response.end();
+
+    let postRequest = 'cmd=_notify-validate';
+    for (const key in body) {
+			const value = queryString.escape(body[key]);
+			postRequest = postRequest + "&" + key + "=" + value;
+    }
+    console.log(postRequest);
+
+    var options = {
+		url: 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr',
+		method: 'POST',
+		headers: {
+			'Connection': 'close'
+		},
+		body: postRequest
+    };
+    try {
+        const result = await requestAsync(options);
+        if (result.statusCode === 200) {
+            if (result.body === 'VERIFIED') {
+                console.log('IPN Verfied');
+            }
+            else if (result.body === ' INVALID') {
+                console.log('IPN Invalid');
+            }
+            else {
+                console.log('Unexpected response');
+            }
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
 });
 
-app.listen(3000, () => console.log('Server started in port 3000'));
+app.listen(3000, () => console.log('Server started in port 3000...'));
